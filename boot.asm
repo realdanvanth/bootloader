0628a0afd39367e02d37911597cb0c8b29aa63cd
realdanvanth <realdanvanth@gmail.com>
2025-12-07 23:19:55 +0530

    steal some code

--- boot.asm
+++ boot.asm
@@ -1,107 +1,33 @@
-bits 16 
+bits 16
 org 0x7c00
-mov si, 0
-call clearscreen
-print:
-  mov ah, 0x0e
-  mov al, [hello+si]
-  int 0x10
-  add si,1
-  cmp byte [hello+si], 0 
-  jne print
-mov si, 0 
-mov al, 0
-call input
-jmp prime
-input: 
-  mov ah, 0x00
-  int 0x16
-  cmp al,0x0D
-  je prime
-  imul si, 10
-  mov ah, 0x0e
-  int 0x10
-  sub al, '0'
-  movzx ax, al
-  add si, ax
-  jmp input
-prime: 
-  mov di, 0
-  mov cx, 1 
+loading:
+  mov si, 0
   .loop:
-  mov ax, si
-  mov dx,0
-  mov bx,cx
-  div bx
-  cmp dx, 0
-  je incdi
-  .continue:
-  inc cx
-  cmp cx, si
-  jle .loop
-  jmp output
-output:
-  cmp di, 2
-  jg print0
-  jle print1
-print1:
-  mov si, 1
-  jmp printnumber
-  hlt
-print0:
-  mov si, 0 
-  jmp printnumber
-incdi:
-  inc di
-  jmp prime.continue
-reverse:
-  imul di, 10
-  mov ax,si
-  mov dx,0
-  mov bx,10
-  div bx
-  mov si, ax
-  add di, dx
-  cmp si, 0 
-  jne reverse
-  mov si, di
-  jmp printnumber
-printnumber:
-  mov ax,si
-  mov dx,0
-  mov bx,10
-  div bx
-  mov si, ax
-  add dl, '0'
-  mov al, dl 
-  mov ah, 0x0e 
+  call delay       
+  mov dh, 12       
+  mov dl, 40 
+  call movcursor
+  mov ah, 0x0e
+  mov al, [load+si]
   int 0x10
-  cmp si, 0
-  jne printnumber
-jmp $
-halt:
-  hlt
-hello:
-  db "Enter the number to check: ", 0
-hellow:
-  db "hello world", 0
-clearscreen:
-    push bp
-    mov bp, sp
-    pusha
-
-    mov ah, 0x07        ; tells BIOS to scroll down window
-    mov al, 0x00           ; clear entire window
-    mov bh, 0x1F            ; white on black
-    mov cx, 0x00        ; specifies top left of screen as (0,0)
-    mov dh, 0x50        ; 18h = 24 rows of chars
-    mov dl, 0x4f        ; 4fh = 79 cols of chars
-    int 0x10        ; calls video interrupt
-
-    popa
-    mov sp, bp
-    pop bp
-    ret
+  inc si
+  cmp byte [load+si],0
+  jne .loop
+  mov si, 0
+  jmp .loop
+movcursor:
+  mov ah,2
+  mov bh, 0
+  int 0x10
+  ret
+delay: 
+  mov ah,0x86
+  mov cx,0x0002    
+  mov dx,0x86A0    
+  int 0x15
+  ret
+load:
+  db "-\|/", 0
 times 510-($-$$) db 0
 dw 0xAA55
 
