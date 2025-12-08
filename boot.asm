32027f5ab36049bb8a08b1bbb5238721cdadef4b
realdanvanth <realdanvanth@gmail.com>
2025-12-08 14:17:49 +0530

    debug

--- boot.asm
+++ boot.asm
@@ -1,33 +1,8 @@
 bits 16
-org 0xc700
-loading:
-  mov si, 0
-  .loop:
-  call delay       
-  mov dh, 12       
-  mov dl, 40 
-  call movcursor
-  mov ah, 0x0e
-  mov al, [load+si]
-  int 0x10
-  inc si
-  cmp byte [load+si],0
-  jne .loop
-  mov si, 0
-  jmp .loop
-movcursor:
-  mov ah,2
-  mov bh, 0
-  int 0x10
-  ret
-delay: 
-  mov ah,0x86
-  mov cx,0x0002    
-  mov dx,0x86A0    
-  int 0x15
-  ret
-load:
-  db "-\|/", 0
+org 0x7c00
+call input
+call printsi
+jmp loading
 printsi:
   mov di, 0
   .loop1:
@@ -85,12 +60,44 @@ clearscreen:
     mov sp, bp
     pop bp
     ret
+loading:
+  mov si, 0
+  .loop:
+  call delay       
+  mov dh, 12       
+  mov dl, 40 
+  call movcursor
+  mov ah, 0x0e
+  mov al, [load+si]
+  int 0x10
+  inc si
+  cmp byte [load+si],0
+  jne .loop
+  mov si, 0
+  jmp .loop
+
+movcursor:
+  mov ah,2
+  mov bh, 0
+  int 0x10
+  ret
+
+delay: 
+  mov ah,0x86
+  mov cx,0x0002    
+  mov dx,0x86A0    
+  int 0x15
+  ret
+
 halt:
   mov ah, 0x0e
   mov al, '@'
   int 0x10
   hlt
   jmp halt
+
+load:
+  db "-\|/", 0
 times 510-($-$$) db 0
 dw 0xAA55
 
